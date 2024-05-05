aeb4df7625f64bd30dfbac2befebcdfd6c555256
Cho Koung Min <rudals123423@gmail.com>
2024-05-05 19:28:06 +0900

    feat: 수강생등록 최종확인과 추가 기능 추가
    수강생 등록시 선택한 내용이 맞는지 최종확인을 진행하고 맞다면 저장 틀리면 처음부터시작

--- src/camp/managemants/StudentManagement.java
+++ src/camp/managemants/StudentManagement.java
@@ -3,7 +3,9 @@ package camp.managemants;
 import camp.model.Student;
 import camp.model.Subject;
 
+import java.sql.SQLOutput;
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StudentManagement extends Management {
@@ -48,9 +50,14 @@ public class StudentManagement extends Management {
     private void addStudentInfo() {
         boolean isEnded = false;
         while (!isEnded) {
+            // 여기에 저장을 subjectList 에 있는 저장된 그 곳의 주소값을 모은 배열로 저장을 해야함 ?
+
             List<Subject> subjects = new ArrayList<>(); // 저장해야할 수강생의 과목 목록 데이터
+//            List<Student> subjects = subjectList;
             System.out.println("수강생 등록 실행 중...");
             String studentName;
+            String[] selectMandatory;
+            String[] selectChoice;
 
             try {
                 studentName = inputStudentName();
@@ -62,31 +69,33 @@ public class StudentManagement extends Management {
 
             // 필수과목 선택과 조건확인
             try {
-                checkSelectMandatory(); // 이걸 어디다 저장해야함
+                selectMandatory = checkSelectMandatory();
             } catch (Exception e) {
                 System.out.println(e.getMessage());
                 continue;
             }
 
-            viewChoiceSubject(); // 선택 과목 조회 메서드
+            // 선택과목 선택과 조건확인
+            try {
+                selectChoice = checkSelectChoice();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
 
-            System.out.println("수강신청한 선택과목 번호를 전부 입력하세요 (2개 이상 ex. 6,7,8)");
-            String selectChoice = sc.next();
-            // 선택과목 체크 기능 추가
+            // 최종체크 메서드
+            try {
+                subjects = finalCheckStudentInfo(studentName, selectMandatory, selectChoice);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
 
+            // 수강생 모델 생성
+            Student student = new Student(sequence(INDEX_TYPE_STUDENT), studentName, subjects);
 
-            // 최종확인
-            System.out.println("선택된 정보가 맞다면 yes, 틀리다면 no 를 입력하세요");
-            String finalCheckStudentInfo = sc.next();
-
-            // 이번에 등록할 학생의 고유번호
-            int thisStudentID = sequence(INDEX_TYPE_STUDENT);
-
-            // yes 를 입력받으면 최정적으로 저장
-            // subjects 에 과목 저장을 구현
-
-            Student student = new Student(thisStudentID, studentName, subjects); // 수강생 모델 생성
-            studentList.add(student); // 모델 리스트에 저장
+            // 모델 리스트에 저장
+            studentList.add(student);
             System.out.println("수강생 관리 화면으로 돌아갑니다.");
             isEnded = true;
         }
@@ -102,6 +111,7 @@ public class StudentManagement extends Management {
         System.out.println("수강생 관리 화면으로 돌아갑니다.");
     }
 
+    // 수강생 이름 입력 확인
     private String inputStudentName(){
         System.out.println("등록할 수강생 이름을 입력하세요.");
         String studentName = sc.next();
@@ -115,61 +125,152 @@ public class StudentManagement extends Management {
         }
     }
 
-    //필수과목 조회
+    //필수과목 목록 조회
     private void viewMandatorySubject(){
         subjectList.stream()
                 .filter(subject -> subject.getSubjectType().equals(SUBJECT_TYPE_MANDATORY))
                 .forEach(subject -> System.out.println("["+ subject.getSubjectId() + "번] " + subject.getSubjectName()));
     }
 
-    // 선택과목 조회
+    // 선택과목 목록 조회
     private void viewChoiceSubject(){
         subjectList.stream()
                 .filter(subject -> subject.getSubjectType().equals(SUBJECT_TYPE_CHOICE))
                 .forEach(subject -> System.out.println("["+ subject.getSubjectId() + "번] " + subject.getSubjectName()));
     }
 
-    // 중복체크와 오름차순정렬
+    // 필수과목 중복체크와 오름차순정렬
     private String[] checkSelectMandatory(){
 
         viewMandatorySubject(); // 필수과목 조회 메서드
 
-        System.out.println("수강청한 필수과목 번호를 전부 입력하세요 (3개 이상 ex. 1,2,3)");
+        System.out.println("\n수강청한 필수과목 번호를 전부 입력하세요 (3개 이상 ex. 1,2,3)");
         String selectMandatory = sc.next();
 
-        String[] str = selectMandatory.split(","); // 구분선을 빼고 저장
+        //구분선 뺴고 저장
+        String[] str = selectMandatory.split(",");
 
         // set 으로 변환했다 돌아옴으로써 중복 제거
         LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>(Arrays.asList(str));
         String[] duplicationCheckedStr = linkedHashSet.toArray(new String[0]);
 
-        Arrays.sort(duplicationCheckedStr); // 오름차순 정렬
+        // 오름차순 정렬
+        Arrays.sort(duplicationCheckedStr);
 
-        return conditionCheck(duplicationCheckedStr);
+        return conditionCheckMandatory(duplicationCheckedStr);
     }
 
-
-    // 입력값이 필수과목 ID 에 해당하는 숫자인지 확인후 조건 확인
-    private String[] conditionCheck(String[] checkSubject){
-        // 1단계 : 필수과목의 id 넘버만 가져온다
-        // 2단계 : 필수과목 id 넘버와 배열의 요소요소를 비교한다
-        // 3단계 : 필수과목 id 넘버와 같은 값들만 남긴 문자열배열을 리턴한다
+    // 필수과목에 맞는 값인지 확인후 맞는 과목의 id만 남김
+    private String[] conditionCheckMandatory(String[] checkSubject){
+        // 입력한 번호가 신청한 필수과목의 id 값과 같은 값만을 저장
         // 진짜 코드 가독성 똥이다..
         Stream<String> checkSub = Arrays.stream(checkSubject)
-                .filter(input -> subjectList.stream().anyMatch(subject -> Objects.equals(subject.getSubjectType(), SUBJECT_TYPE_MANDATORY) && Objects.equals(Integer.parseInt(input), subject.getSubjectId())));
+                .filter(input -> subjectList.stream()
+                        .anyMatch(subject -> Objects.equals(subject.getSubjectType(), SUBJECT_TYPE_MANDATORY)
+                                && Objects.equals(Integer.parseInt(input), subject.getSubjectId())));
 
         // String 타입을 String[] 로 변환하여 리턴
         String[] checkedSubject = checkSub.toArray(String[]::new);
 
-        // 필수 과목 조건인 3개인상인지 확인
+        // 필수 과목 조건 확인
         if (checkedSubject.length > 2) {
-            System.out.println("선택 필수 과목 : " + Arrays.toString(checkedSubject)); // 일단 test 코드
+//            System.out.println("신청한 필수 과목 : " + Arrays.toString(checkedSubject)); // test 코드
             return checkedSubject;
         } else {
-            System.out.println("신청한 필수과목이 3과목 미만입니다. 재입력해주세요.");
+            System.out.println("\n신청한 필수과목이 3과목 미만입니다. 재입력해주세요.\n");
             return checkSelectMandatory();
         }
     }
+
+    // 선택과목 중복체크와 오름차순정렬
+    private String[] checkSelectChoice(){
+
+        viewChoiceSubject(); // 선택과목 조회 메서드
+
+        System.out.println("\n수강신청한 선택과목 번호를 전부 입력하세요 (2개 이상 ex. 6,7,8)");
+        String selectChoice = sc.next();
+
+        // 구분선을 빼고 저장
+        String[] str = selectChoice.split(",");
+
+        // set 으로 변환했다 돌아옴으로써 중복 제거
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>(Arrays.asList(str));
+        String[] duplicationCheckedStr = linkedHashSet.toArray(new String[0]);
+
+        // 오름차순 정렬
+        Arrays.sort(duplicationCheckedStr);
+
+        return conditionCheckChoice(duplicationCheckedStr);
+    }
+
+    // 선택과목에 맞는 값인지 확인후 맞는 과목의 id만 남김
+    private String[] conditionCheckChoice(String[] checkSubject){
+        // 입력한 번호가 신청한 선택과목의 id 값과 같은 값만을 저장
+        Stream<String> checkSub = Arrays.stream(checkSubject)
+                .filter(input -> subjectList.stream()
+                        .anyMatch(subject -> Objects.equals(subject.getSubjectType(), SUBJECT_TYPE_CHOICE)
+                                && Objects.equals(Integer.parseInt(input), subject.getSubjectId())));
+
+        // String 타입을 String[] 로 변환하여 리턴
+        String[] checkedSubject = checkSub.toArray(String[]::new);
+
+        // 선택 과목 조건 확인
+        if (checkedSubject.length > 1) {
+//            System.out.println("신청한 선택 과목 : " + Arrays.toString(checkedSubject)); // test 코드
+            return checkedSubject;
+        } else {
+            System.out.println("\n신청한 선택과목이 2과목 미만입니다. 재입력해주세요.\n");
+            return checkSelectChoice();
+        }
+    }
+
+    private List<Subject> finalCheckStudentInfo(String studentName, String[] selectMandatory, String[] selectChoice){
+        System.out.println("이름 : " + studentName);
+
+        // 두개의 문자열 배열을 하나로 합침
+        String[] selectSubject = new String[selectMandatory.length + selectChoice.length];
+        System.arraycopy(selectMandatory, 0, selectSubject, 0, selectMandatory.length);
+        System.arraycopy(selectChoice, 0, selectSubject, selectMandatory.length, selectChoice.length);
+
+        // 선택한 과목의 정보를 담을 리스트 생성
+        List<Subject> selectedSubjects = new ArrayList<>();
+
+        // 선택한 과목의 고유 번호를 순회하면서 실제 Subject 객체를 찾아 리스트에 추가
+        for (String subjectId : selectSubject) {
+            int id = Integer.parseInt(subjectId);
+
+            // subjectList 에서 고유 번호에 해당하는 Subject 객체를 찾아내어 selectedSubjects 리스트에 추가
+            Subject selectedSubject = subjectList.stream()
+                    .filter(subject -> subject.getSubjectId() == id)
+                    .findFirst()
+                    .orElse(null);
+
+            // 찾은 Subject 객체가 null 이 아니면 리스트에 추가
+            if (selectedSubject != null) {
+                selectedSubjects.add(selectedSubject);
+            }
+        }
+
+        // 최종 확인을 위한 선택한 내용 출력
+        for (Subject subject : selectedSubjects) {
+            int subjectId = subject.getSubjectId();
+            String subjectName = subject.getSubjectName();
+            String subjectType = subject.getSubjectType();
+
+            System.out.println("선택한 과목 번호: " + subjectId
+                    + ", 선택한 과목 타입: " + subjectType
+                    + ", 선택한 과목 이름: [ " + subjectName + " ]");
+        }
+
+        System.out.println("선택된 정보가 맞다면 yes, 틀리다면 no 를 입력하세요");
+        String finalCheckMsg = sc.next();
+        if ("yes".equalsIgnoreCase(finalCheckMsg)) {
+            return selectedSubjects;
+        } else {
+            addStudentInfo();
+        }return new ArrayList<>();// 잘못되면 초기화
+    }
+
 
     /**
      * 수강생의 과목 목록 출력(수강생이 존재하지 않으면 예외처리)
